861275e6a0d1810df231501d0d4fe82eef8d36b1
oceannut <oceannut@163.com>
2014-05-14 15:49:54 +0800

    20150514

---
+++ ThinkInBio.Cully.WSL/ITeamWcfService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+using ThinkInBio.Cully;
+
+namespace ThinkInBio.Cully.WSL
+{
+
+    [ServiceContract]
+    public interface ITeamWcfService
+    {
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            BodyStyle = WebMessageBodyStyle.WrappedRequest,
+            UriTemplate = "/project/{name}/",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json)]
+        Project SaveProject(string name, string description);
+
+    }
+
+}
