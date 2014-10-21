697a517ade04b3ca60ea3b4c79c850565138fea9
oceannut <oceannut@163.com>
2014-10-21 15:57:38 +0800

    20141021

--- cully/app/cully/js/index-controllers.js
+++ cully/app/cully/js/index-controllers.js
@@ -6,15 +6,16 @@ define(function (require) {
 
     require('../../../static/js/configs');
     require('../../../static/js/events');
+    require('../../../static/js/utils');
     require('../../common/js/biz-notification-services');
     require('../../auth/js/auth-models');
     require('./client-services');
     require('./calendar-services');
 
-    angular.module('index.controllers', ['configs', 'events', 'auth.models', 'bizNotification.services', 'client.services'])
-        .controller('IndexCtrl', ['$scope', '$location', '$log', '$interval', 'currentUser', 'eventbus', 'appName',
+    angular.module('index.controllers', ['configs', 'events', 'utils', 'auth.models', 'bizNotification.services', 'client.services'])
+        .controller('IndexCtrl', ['$scope', '$location', '$log', '$interval', 'dateUtil', 'currentUser', 'eventbus', 'appName',
                                     'UntreatedBizNotificationListService', 'CalendarOfCautionService', 'localStorageUtil',
-            function ($scope, $location, $log, $interval, currentUser, eventbus, appName,
+            function ($scope, $location, $log, $interval, dateUtil, currentUser, eventbus, appName,
                         UntreatedBizNotificationListService, CalendarOfCautionService, localStorageUtil) {
 
                 var homeNav = {
@@ -47,7 +48,7 @@ define(function (require) {
                     "url": "/#/",
                     "active": ""
                 };
-                var untreatedBizNotificationCountTimer; //刷新通知个数的时间定义。
+                var cautionTimer;
                 var cautionInterval;
 
                 function loadUntreatedNotificationCount() {
@@ -66,13 +67,19 @@ define(function (require) {
                                 });
                 }
 
-                function loadCautionCalendarList() {
+                function loadCautionCalendarList(refresh) {
                     $scope.calendarList.length = 0;
                     var date = new Date();
+                    var year = date.getFullYear();
+                    var month = date.getMonth() + 1;
+                    var day = date.getDate();
+                    var hour = date.getHours();
+                    var minute = date.getMinutes();
+                    var week = date.getDay();
                     CalendarOfCautionService.query({
-                        'year': date.getFullYear(),
-                        'month': date.getMonth() + 1,
-                        'day': date.getDate(),
+                        'year': year,
+                        'month': month,
+                        'day': day,
                         'user': currentUser.getUsername()
                     })
                     .$promise
@@ -80,9 +87,95 @@ define(function (require) {
                             if (angular.isArray(result)) {
                                 var len = result.length;
                                 if (len > 0) {
-                                    $scope.isCautionClosed = false;
+                                    if (!refresh) {
+                                        $scope.isCautionClosed = false;
+                                    }
+                                    var playMusic = false;
                                     for (var i = 0; i < len; i++) {
-                                        $scope.calendarList.push(result[i]);
+                                        var item = result[i];
+                                        var appointed = dateUtil.jsonToDate(item.Appointed);
+                                        var caution = dateUtil.jsonToDate(item.Caution);
+                                        switch (item.Repeat) {
+                                            case 0:
+                                                item.repeatDesc = "当日";
+                                                if (year === appointed.getFullYear()
+                                                    && month === (appointed.getMonth() + 1)
+                                                    && day === appointed.getDate()
+                                                    && hour === caution.getHours()
+                                                    && minute === caution.getMinutes()) {
+                                                    playMusic = true;
+                                                }
+                                                break;
+                                            case 1:
+                                                item.repeatDesc = "每天";
+                                                if (hour === caution.getHours()
+                                                    && minute === caution.getMinutes()) {
+                                                    playMusic = true;
+                                                }
+                                                break;
+                                            case 2:
+                                                item.repeatDesc = "每周";
+                                                if (week === appointed.getDay()
+                                                    && hour === caution.getHours()
+                                                    && minute === caution.getMinutes()) {
+                                                    playMusic = true;
+                                                }
+                                                break;
+                                            case 3:
+                                                item.repeatDesc = "每月";
+                                                if (day === appointed.getDate()
+                                                    && hour === caution.getHours()
+                                                    && minute === caution.getMinutes()) {
+                                                    playMusic = true;
+                                                }
+                                                break;
+                                            case 5:
+                                                item.repeatDesc = "星期一至星期五";
+                                                if (day >= 1 && day <= 5
+                                                    && hour === caution.getHours()
+                                                    && minute === caution.getMinutes()) {
+                                                    playMusic = true;
+                                                }
+                                                break;
+                                            default:
+                                                var aWeek = item.Repeat - 10;
+                                                switch (aWeek) {
+                                                    case 0:
+                                                        item.repeatDesc = "星期日";
+                                                        break;
+                                                    case 1:
+                                                        item.repeatDesc = "星期一";
+                                                        break;
+                                                    case 2:
+                                                        item.repeatDesc = "星期二";
+                                                        break;
+                                                    case 3:
+                                                        item.repeatDesc = "星期三";
+                                                        break;
+                                                    case 4:
+                                                        item.repeatDesc = "星期四";
+                                                        break;
+                                                    case 5:
+                                                        item.repeatDesc = "星期五";
+                                                        break;
+                                                    case 6:
+                                                        item.repeatDesc = "星期六";
+                                                        break;
+                                                }
+                                                if (day === aWeek
+                                                    && hour === caution.getHours()
+                                                    && minute === caution.getMinutes()) {
+                                                    playMusic = true;
+                                                }
+                                                break;
+                                        }
+                                        $scope.calendarList.push(item);
+                                    }
+                                    if (playMusic) {
+                                        var audio = document.getElementById("cautionCalendarAudio");
+                                        if (audio !== undefined && audio !== null) {
+                                            audio.play();
+                                        }
                                     }
                                 }
                             }
@@ -115,22 +208,23 @@ define(function (require) {
                         }
 
                         loadUntreatedNotificationCount();
-                        loadCautionCalendarList();
+                        loadCautionCalendarList(false);
                         cautionInterval = localStorageUtil.getUserData(currentUser.getUsername(), localStorageUtil.caution, localStorageUtil.cautionDV, true);
                         if (cautionInterval > 0) {
-                            untreatedBizNotificationCountTimer = $interval(function () {
+                            cautionTimer = $interval(function () {
                                 loadUntreatedNotificationCount();
-                                loadCautionCalendarList();
+                                loadCautionCalendarList(true);
                             }, cautionInterval * 1000);
                         }
 
                     });
                     eventbus.subscribe("userSignOut", function (e, data) {
                         $scope.makeNavbarVisible();
+                        $scope.isCautionClosed = true;
                         $scope.loginUser = undefined;
 
-                        if (untreatedBizNotificationCountTimer !== undefined && untreatedBizNotificationCountTimer !== null) {
-                            $interval.cancel(untreatedBizNotificationCountTimer);
+                        if (cautionTimer !== undefined && cautionTimer !== null) {
+                            $interval.cancel(cautionTimer);
                         }
 
                     });
@@ -142,12 +236,13 @@ define(function (require) {
                     });
                     eventbus.subscribe("clientUserDataChanged", function (e, data) {
                         var interval = localStorageUtil.getUserData(currentUser.getUsername(), localStorageUtil.caution, localStorageUtil.cautionDV, true);
-                        if (cautionInterval != interval && untreatedBizNotificationCountTimer !== undefined && untreatedBizNotificationCountTimer !== null) {
+                        if (cautionInterval != interval && cautionTimer !== undefined && cautionTimer !== null) {
                             cautionInterval = interval;
-                            $interval.cancel(untreatedBizNotificationCountTimer);
+                            $interval.cancel(cautionTimer);
                             if (cautionInterval > 0) {
-                                untreatedBizNotificationCountTimer = $interval(function () {
+                                cautionTimer = $interval(function () {
                                     loadUntreatedNotificationCount();
+                                    loadCautionCalendarList(true);
                                 }, cautionInterval * 1000);
                             }
                         }
@@ -165,8 +260,8 @@ define(function (require) {
                     ];
 
                     $scope.$on("$destroy", function (event) {
-                        if (untreatedBizNotificationCountTimer !== undefined && untreatedBizNotificationCountTimer !== null) {
-                            $interval.cancel(untreatedBizNotificationCountTimer);
+                        if (cautionTimer !== undefined && cautionTimer !== null) {
+                            $interval.cancel(cautionTimer);
                         }
                     });
 
